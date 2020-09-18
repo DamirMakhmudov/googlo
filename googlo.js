0907a0a67306bad7c576d25b302fa8a86e690700
Damir <d.makhmudov@kazan.csoft.ru>
2020-09-18 15:50:02 +0300

    first

--- googlo.js
+++ googlo.js
@@ -1,7 +1,7 @@
 var at, ci, cs, rt;
 
 function getCredentials(){
-    console.log('---getCredentials---2')
+    console.log('---getCredentials---3')
     google.script.run.withSuccessHandler(
       function (credentials){
         console.log(credentials);
