5c8a03dbe4763250be5ce9da511164d68e4da717
Damir <d.makhmudov@kazan.csoft.ru>
2020-12-10 09:45:38 +0300

    no

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-googlo
-=
+# googlo
 googlo is simple way to get access to google api from common javascript inside your web page  
 all your need is 3 authorizations parameters like: client id, client secert and refresh token  
 make sure authorization parameters on [google developers console](https://console.developers.google.com)
 
-how to
-=
+## how to
 insert next code to your html
 ```js
 <script onload="getAccessToken()" src="https://rawcdn.githack.com/DamirMakhmudov/googlo/2093ba895c158f39297533dfe0856c31e2b07f5f/googlo.js" async defer></script>
+```
+ ыщьу
+ =
--- googlo.js
+++ googlo.js
@@ -24,41 +24,157 @@ async function getAccessToken(){
     method: 'POST',
     body:JSON.stringify(payload)
   });
-  let resJson = await res.json();
-  at = resJson.access_token;
+  at = (await res.json()).access_token;
 }
 
 /* READ */
 
-async function readRows(spreadSheetId, sheetName, rowS, rowE){ 
-  await getAccessToken();
-  let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!A${rowS}:ZZ${rowE}?majorDimension=ROWS`;
-  let res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + at
-    }
-  });
-  let resJson = await res.json();
-  return resJson.values;
+async function jsonerO(headers, data){
+  let rows = [];
+  data.forEach((row, i) => {
+    let obj = {};
+    headers[0].forEach((col, j) => {
+      obj[col] = row[j]
+    });
+    rows.push(obj);
+  })
+  return rows
 }
 
-// using batch
-async function readRange(spreadSheetId, sheetIndex, rowS, rowE, colS, colE){
+async function readProperties(spreadSheetId){ 
+  try{
+    await getAccessToken();
+    let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}?includeGridData=true`;
+    let res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${at}`
+      }
+    });
+    return (await res.json())
+  }catch(e){
+    console.error(e);
+  }
+}
+
+async function getSheets(spreadSheetId){
+  try{
+    await getAccessToken();
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}?includeGridData=false`;
+    let res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${at}`
+      }
+    });
+    return (await res.json()).sheets
+  }catch(e){
+    console.error(e);
+  }
+}
+
+async function readRows2(spreadSheetId, sheetName, rowS, rowE){ 
+  try{
+    await getAccessToken();
+    let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!1:1?majorDimension=ROWS`;
+    let res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${at}`
+      }
+    });
+    let headers = (await res.json()).values;
+    // console.log('headers', headers);
+
+    rowE = rowS + rowE -1;
+    let url2 = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!A${rowS}:ZZ${rowE}?majorDimension=ROWS`;
+    let res2 = await fetch(url2, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${at}`
+      }
+    });
+    let data = (await res2.json()).values;
+    // console.log('data', data);
+
+    return (await jsonerO(headers, data))
+  }catch(e){
+    console.error(e);
+  }
+}
+
+async function readRows(spreadSheetId, sheetName, rowS, rowE){ 
+  try{
+    await getAccessToken();
+    let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!A${rowS}:ZZ${rowE}?majorDimension=ROWS`;
+    let res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${at}`
+      }
+    });
+    return (await res.json()).values
+  }catch(e){
+    console.error(e);
+  }
+}
+
+//!!!
+async function readRange2(spreadSheetId, sheetName, rowS, rowE){
+  try{
+    await getAccessToken();
+
+    var dataFilterHeader = {
+      "a1Range": `${sheetName}!1:1`
+    };
+
+    var dataFilter = {
+      "a1Range": `${sheetName}!A${rowS}:ZZ${rowS+rowE}`
+    };
+
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values:batchGetByDataFilter`;
+    var req = {
+      "dataFilters": [
+        dataFilterHeader,
+        dataFilter
+      ],
+      "majorDimension": "ROWS",
+      "valueRenderOption": "FORMATTED_VALUE"
+      // "dateTimeRenderOption": ""
+    };
+    let res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${at}`
+      },
+      body: JSON.stringify(req)
+    });
+    let resJson = (await res.json());
+    console.log(resJson);
+    return
+    console.log(resJson[0].valueRange.values);
+    console.log(resJson[1].valueRange.values);
+    return jsonerO(resJson[1].valueRange.values, resJson[0].valueRange.values)
+  }catch(e){
+    console.error(e);
+  }
+}
+
+async function readRange(spreadSheetId, sheetIndex, rowS, colS, rowE, colE){
   try{
     await getAccessToken();
     var grid = {
       "sheetId": sheetIndex,
       "startRowIndex": rowS - 1,
-      "endRowIndex": rowE,
+      "endRowIndex": rowS + rowE,
       "startColumnIndex": colS - 1,
-      "endColumnIndex": colE
+      "endColumnIndex": colS + colE
     }
     var dataFilter = {
       "gridRange": grid
     };
-    
-    var url = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetId + "/values:batchGetByDataFilter";
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values:batchGetByDataFilter`;
     var req = {
       "dataFilters": [
         dataFilter
@@ -70,35 +186,12 @@ async function readRange(spreadSheetId, sheetIndex, rowS, rowE, colS, colE){
     let res = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + at
+        'Authorization': `Bearer ${at}`
       },
       body: JSON.stringify(req)
     });
-    let resJson = await res.json();
-    return resJson.valueRanges[0].valueRange.values;
-  }catch(e){
-    console.error(e);
-  }
-}
-
-async function getSheets(spreadSheetId){
-  try{
-    await getAccessToken();
-    var url = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetId;
-    
-    var req = {
-      "spreadsheetId":spreadSheetId
-    };
-    let res = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + at
-      }
-    });
-    let resJson = await res.json();
-    return resJson.sheets
+    // let resJson = await res.json();
+    return (await res.json()).valueRanges[0].valueRange.values
   }catch(e){
     console.error(e);
   }
@@ -106,28 +199,27 @@ async function getSheets(spreadSheetId){
 
 /* WRITE */
 
-async function writeRows(spreadSheetId, sheetName, rowS, rowE, values){
+async function appendRows(spreadSheetId, sheetName, values){
   await getAccessToken();
-  var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!A${rowS}:ZZ${rowE}?valueInputOption=USER_ENTERED`;
-  var obj = {
-    "range": `${sheetName}!A${rowS}:ZZ${rowE}`,
-    "majorDimension": "ROWS",
+  var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}:append?valueInputOption=USER_ENTERED`;
+  var req = {
     "values": values
   }   
   let res = await fetch(url, {
-    method: 'PUT',
+    method: 'POST',
     headers: {
-      'Authorization': 'Bearer ' + at
+      'Authorization': `Bearer ${at}`
     },
-    body:JSON.stringify(obj)
+    body: JSON.stringify(req)
   });
-  return await res.json();
+  let resJson = await res.json();
+  return resJson;
 }
 
 async function batchUpdate(spreadSheetId, requests){
   try{
     await getAccessToken();
-    var url = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadSheetId + ":batchUpdate";
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}:batchUpdate`;
     var req = {
       "requests": requests,
       "includeSpreadsheetInResponse": false
@@ -140,7 +232,7 @@ async function batchUpdate(spreadSheetId, requests){
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + at
+        'Authorization': `Bearer ${at}`
       },
       body: JSON.stringify(req)
     });
@@ -150,38 +242,37 @@ async function batchUpdate(spreadSheetId, requests){
   }
 }
 
-function pasteO(sheetIndex, row, column, value){
+function pasteData(sheetIndex, row, column, value){
   return {
-      "pasteData":{
-        "coordinate": {
-          "sheetId": sheetIndex,
-          "rowIndex": row-1,
-          "columnIndex": column-1
-        },
-      "data": value,
-      "type": "PASTE_NORMAL",
-      "delimiter": "#"
-      // "html": true
-    }
-  }
-}
-
-function insertRangeO(sheetIndex, row, count){
-  return {
-    "insertRange":{
-      "range":{
+    "pasteData":{
+      "coordinate": {
         "sheetId": sheetIndex,
-        "startRowIndex": row - 1,
-        "endRowIndex": row + count - 1,
-        "startColumnIndex": 0,
-        "endColumnIndex": 100
+        "rowIndex": row-1,
+        "columnIndex": column-1
       },
-      "shiftDimension":"ROWS"
+    "data": value,
+    "type": "PASTE_NORMAL",
+    "delimiter": "#"
+    // "html": true
     }
   }
 }
 
-function replaceO(sheetIndex, input, output){
+function insertDimension(sheetIndex, row, count){
+  return {
+    "insertDimension":{
+      "range": {
+        "sheetId": sheetIndex,
+        "dimension": "ROWS",
+        "startIndex": row,
+        "endIndex": row + count
+      },
+      "inheritFromBefore": true
+    }
+  }
+}
+
+function findReplace(sheetIndex, input, output){
   return {
     "findReplace":{
       "find": input,
@@ -209,8 +300,7 @@ async function batchUpdateValues(spreadSheetId, requests){
     let res = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + at
+        'Authorization': `Bearer ${at}`
       },
       body: JSON.stringify(req)
     })
@@ -220,12 +310,32 @@ async function batchUpdateValues(spreadSheetId, requests){
   }
 }
 
-function prepareO(sheetName, row, obj){
+function valueRange(sheetName, row, obj){
   return {
-    "range": `${sheetName}!A${row}:ZZ${row}`,
+    "range": `${sheetName}!A${row}`,
     "majorDimension": "ROWS",
-    "values": [
-      obj  
-    ]
+    "values": [obj]
   }
+}
+
+async function writeRows(spreadSheetId, sheetName, rowS, values){
+  await getAccessToken();
+  rowE = rowS + values.length-1;
+  // var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!${rowS}:${rowE}?valueInputOption=USER_ENTERED`;
+  var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${sheetName}!A${rowS}?valueInputOption=USER_ENTERED`;
+  var req = {
+    // "range": `${sheetName}!${rowS}:${rowE}`,
+    "range": `${sheetName}!A${rowS}`,
+    "majorDimension": "ROWS",
+    "values": values
+  }   
+  let res = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${at}`
+    },
+    body:JSON.stringify(req)
+  });
+  let resJson = await res.json();
+  return resJson;
 }
